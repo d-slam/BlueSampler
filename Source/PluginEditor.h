24345a6c5d41e291deb0207d77f983fbfa12dacd
Dami <71833645+d-slam@users.noreply.github.com>
2020-09-25 19:35:22 +0200

    Add files via upload

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -14,30 +14,31 @@
 //==============================================================================
 /**
 */
-class BlueSamplerAudioProcessorEditor  : public juce::AudioProcessorEditor, public FileDragAndDropTarget
+class BlueSamplerAudioProcessorEditor : public juce::AudioProcessorEditor, public FileDragAndDropTarget, public Slider::Listener
 {
 public:
-    BlueSamplerAudioProcessorEditor (BlueSamplerAudioProcessor&);
-    ~BlueSamplerAudioProcessorEditor() override;
+	BlueSamplerAudioProcessorEditor(BlueSamplerAudioProcessor&);
+	~BlueSamplerAudioProcessorEditor() override;
 
-    //==============================================================================
-    void paint (juce::Graphics&) override;
-    void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics&) override;
+	void resized() override;
 
-    bool isInterestedInFileDrag(const StringArray& files) override;
-    void filesDropped(const StringArray& files, int x, int y) override;
+	bool isInterestedInFileDrag(const StringArray& files) override;
+	void filesDropped(const StringArray& files, int x, int y) override;
+	void sliderValueChanged(Slider* slider) override;
 
 private:
 
-    TextButton mLoadButton { "Load" };
-    std::vector<float> mAudioPoints;
-    bool mShouldBePainting = false;
+	TextButton mLoadButton{ "Load" };
+	std::vector<float> mAudioPoints;
+	bool mShouldBePainting = false;
 
-    Slider mAttackSlider, mDecaySlider, mSustainSlider, mReleaseSlider;
+	Slider mAttackSlider, mDecaySlider, mSustainSlider, mReleaseSlider;
 
-    Label mAttackLabel, mDecayLabel, mSustainLabel, mReleaseLabel;
+	Label mAttackLabel, mDecayLabel, mSustainLabel, mReleaseLabel;
 
-    BlueSamplerAudioProcessor& audioProcessor;
+	BlueSamplerAudioProcessor& audioProcessor;
 
-    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (BlueSamplerAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(BlueSamplerAudioProcessorEditor)
 };
